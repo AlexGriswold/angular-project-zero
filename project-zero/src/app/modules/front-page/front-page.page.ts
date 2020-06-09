dd0e51d3dad47956c0044e3a7f3b61324e8d8989
Alex G <alex.griswold.developer@gmail.com>
2020-06-09 16:53:20 -0400

    Working on front page typer

--- project-zero/src/app/modules/front-page/front-page.page.ts
+++ project-zero/src/app/modules/front-page/front-page.page.ts
@@ -1,14 +1,36 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
+import { Observable, interval, Subscription } from 'rxjs';
+
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-front-page',
   templateUrl: './front-page.page.html',
   styleUrls: ['./front-page.page.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FrontPageComponent {
+export class FrontPageComponent implements AfterViewInit, OnDestroy {
+
+  content = 'Test content';
+  currentContent = '';
+  intervalObservable = new Observable;
+
+  takeFourNumbers: Observable<number>;
+  numberSub: Subscription;
 
   constructor(
-
   ) { }
+
+  ngAfterViewInit(): void {
+    const numbers = interval(100);
+    this.takeFourNumbers = numbers.pipe(take(this.content.length));
+    this.numberSub = this.takeFourNumbers.subscribe(x => {
+      this.currentContent += this.content[x];
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.numberSub.unsubscribe();
+  }
+
 }
